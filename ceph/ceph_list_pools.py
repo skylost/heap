a8139bf8815e612444392dc11cd9b4b947ae3c75
weezhard <skylostft@laposte.net>
2016-02-07 00:41:55 +0100

    add script list pools

---
+++ ceph/ceph_list_pools.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import rados, sys
+
+try:
+  cluster = rados.Rados(conffile = '/etc/ceph/ceph.conf', conf = dict (keyring = '/etc/ceph/ceph.client.admin.keyring'))
+except TypeError as e:
+  print 'Argument validation error: ', e
+  raise e
+
+try:
+  cluster.connect()
+except Exception as e:
+  print "connection error: ", e
+  raise e
+
+pools = cluster.list_pools()
+for pool in pools:
+  print pool
+
+cluster.shutdown()
